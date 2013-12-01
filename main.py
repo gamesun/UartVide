4e228d1a7fe019502a65220731669016be9dc901
gamesun <gamesunyt@gmail.com>
2013-12-02 00:31:22 +0900

    use icon32 for linux

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ import serial
 # import time
 from wx.lib.wordwrap import wordwrap
 import itertools
-import icon32
+import icon32, icon16
 import pkg_resources
 import zipfile
 from cStringIO import StringIO
@@ -181,15 +181,17 @@ class MyApp(wx.App):
     def OnInit(self):
         self.frame = ui.MyFrame(None, wx.ID_ANY, "")
 
-        my_data = pkg_resources.resource_string(__name__,"library.zip")
-        filezip = StringIO(my_data)
-        zf = zipfile.ZipFile(filezip)
-        data = zf.read("media/icon16.ico")
-#         self.frame.SetIcon(icon16.geticon16Icon())
-        
-        icon = wx.EmptyIcon()
-        icon.CopyFromBitmap(wx.ImageFromStream(StringIO(data), wx.BITMAP_TYPE_ICO).ConvertToBitmap())
-        self.frame.SetIcon(icon)
+        if sys.platform == 'win32':
+            my_data = pkg_resources.resource_string(__name__,"library.zip")
+            filezip = StringIO(my_data)
+            zf = zipfile.ZipFile(filezip)
+            data = zf.read("media/icon16.ico")
+            icon = wx.EmptyIcon()
+            icon.CopyFromBitmap(wx.ImageFromStream(StringIO(data), wx.BITMAP_TYPE_ICO).ConvertToBitmap())
+            self.frame.SetIcon(icon)                        # for the app's title of main window
+            self.frame.SetIcon(icon32.geticon32Icon())      # for the app's task bar
+        elif sys.platform.startswith('linux'):
+            self.frame.SetIcon(icon32.geticon32Icon())
         
 #         self.frame.SetIcon(wx.Icon("media\icon16.ico", wx.BITMAP_TYPE_ICO, 16, 16))
         
