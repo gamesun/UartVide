8e46f57c3aacbb03c2de46d754756b005d5afbd1
gamesun <gamesunyt@gmail.com>
2018-12-07 17:41:36 +0800

    cx-Freeze scripts

---
+++ make_cxFreeze_exe_py35.bat
@@ -0,0 +1,4 @@
+@echo off
+D:\Python\Python35-32\python.exe setup_cxFreeze_qt5.py build_exe
+
+pause
---
+++ make_cxFreeze_exe_py37.bat
@@ -0,0 +1,4 @@
+@echo off
+D:\Python\Python37-32\python.exe setup_cxFreeze_qt5.py build_exe
+
+pause
