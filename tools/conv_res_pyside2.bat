c4233496302d0981b002fddf21b5402f4397e815
gamesun <gamesunyt@gmail.com>
2021-05-29 23:13:20 +0800

    fix path

--- tools/conv_res_pyside2.bat
+++ tools/conv_res_pyside2.bat
@@ -1,4 +1,4 @@
 @echo off
-D:\Python\Python38\Scripts\pyside2-rcc.exe ../res/resources.qrc -o ../src/resources_pyside2.py
+D:\Python\Python38-32\Scripts\pyside2-rcc.exe ../res/resources.qrc -o ../src/resources_pyside2.py
 
-pause
+rem pause
