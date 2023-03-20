fa990f4a57d55a0f472b59d0d755c9489ff6cbe4
gamesun <gamesunyt@gmail.com>
2023-03-21 06:20:14 +0800

    Update copyright year to 2023

--- src/except_logger.py
+++ src/except_logger.py
@@ -3,7 +3,7 @@
 #
 #############################################################################
 ##
-## Copyright (c) 2013-2022, gamesun
+## Copyright (c) 2013-2023, gamesun
 ## All right reserved.
 ##
 ## This file is part of UartVide(MyTerm).
